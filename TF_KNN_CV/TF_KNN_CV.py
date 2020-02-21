3a584e64c7b872818de72a2ca66cfada7ded8ba4
Sourav Barua <sourav.shr6@gmail.com>
2020-02-21 18:18:02 -0330

    Edited the distance function 1

--- TF_KNN_CV/TF_KNN_CV.py
+++ TF_KNN_CV/TF_KNN_CV.py
@@ -3,6 +3,45 @@ import csv
 import sys
 import scipy.stats as stats
 
+## Distance Function 1
+def CalculateDistance(aminoAcidSeq1, aminoAcidSeq2):
+    #print(aminoAcidSeq1,aminoAcidSeq2)
+    distance = 0
+    try:
+        if aminoAcidSeq1[2] != aminoAcidSeq2[2]:
+            distance+=1
+        if aminoAcidSeq1[4] != aminoAcidSeq2[4]:
+            distance+=1
+        if aminoAcidSeq1[5] != aminoAcidSeq2[5]:
+            distance+=1
+        if aminoAcidSeq1[24] != aminoAcidSeq2[24]:
+            distance+=1
+        if aminoAcidSeq1[30] != aminoAcidSeq2[30]:
+            distance+=1
+        if aminoAcidSeq1[43] != aminoAcidSeq2[43]:
+            distance+=1
+        if aminoAcidSeq1[45] != aminoAcidSeq2[45]:
+            distance+=1
+        if aminoAcidSeq1[46] != aminoAcidSeq2[46]:
+            distance+=1
+        if aminoAcidSeq1[47] != aminoAcidSeq2[47]:
+            distance+=1
+        if aminoAcidSeq1[49] != aminoAcidSeq2[49]:
+            distance+=1
+        if aminoAcidSeq1[50] != aminoAcidSeq2[50]:
+            distance+=1
+        if aminoAcidSeq1[52] != aminoAcidSeq2[52]:
+            distance+=1
+        if aminoAcidSeq1[53] != aminoAcidSeq2[53]:
+            distance+=1
+        if aminoAcidSeq1[54] != aminoAcidSeq2[54]:
+            distance+=1
+        if (aminoAcidSeq1[56] != aminoAcidSeq2[56]):
+            distance+=1
+    except IndexError:
+        print("Amino Acid Sequence length less than 57 characters")  
+    return distance
+
 ## Reading the file names from command-line arguments
 ## X_train.txt
 ## Y_train.txt
